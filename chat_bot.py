3dd118e0092c3c8634dff8ea7cc9291b05993e05
Guche02 <loozasubedy998@gmail.com>
2025-03-03 14:49:20 +0545

    login vectordb created, chatbot code modifed for two llms

--- chat_bot.py
+++ chat_bot.py
@@ -1,12 +1,12 @@
 from chromadb import PersistentClient  # type: ignore
-from utils.prompt import get_prompt, get_validation_prompt
-from utils.llm import run_chat
+from utils.prompt import get_prompt, get_validation_prompt, get_query_category
+from utils.llm import run_chat_login, run_chat_others
 from sentence_transformers import SentenceTransformer   # type: ignore
  
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-client = PersistentClient(path="vectordb/")
-collection = client.get_collection(name="knowlage_base")
+client = PersistentClient(path="new_vector_db/")
+collection = client.get_collection(name="login_related_chunks")
 
 print(collection.count())
 
@@ -17,16 +17,22 @@ def chatbot(question):
     n_results=3
     )   
     print(f"Retrived context: {context}")
+    prompt_for_type = get_query_category(question)
     prompt = get_prompt(context['documents'],question)
-    response = run_chat(prompt)
+    prompt_type = run_chat_login(prompt_for_type)
+    print(f"Prompt type: {prompt_type}")
+    if prompt_type == "login":
+        response = run_chat_login(prompt)
+    else:
+        response = run_chat_others(prompt)
     print(f"Initial Response: {response}")
 
     validation_prompt = get_validation_prompt(response, question)
     print(f"Validation prompt: {validation_prompt}")
-    validation_result = run_chat(validation_prompt)
+    validation_result = run_chat_others(validation_prompt)
     print(f"Final result: {validation_result}")
 
     return validation_result
 
-print(chatbot("How do I change my name on my policy ?")) 
-# print(chatbot("How does an agent sell their plans?")) 
+# print(chatbot("How do I purchase an insurance?")) 
+# print(chatbot("I forgot my password, how do I reset it?"))
--- utils/llm.py
+++ utils/llm.py
@@ -4,16 +4,21 @@ import os
 from dotenv import load_dotenv  # type: ignore
 load_dotenv()
 
-llm = ChatGroq(
+llm_login = ChatGroq(
     model="llama-3.1-8b-instant",
     temperature=0.3,
     max_retries=3,
 )
 
-def run_chat(prompt):
+llm_others = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0.3,
+    max_retries=3,
+)
+
+def run_chat_login(prompt):
     try:
-        response = llm.invoke(prompt)
-        
+        response = llm_login.invoke(prompt)
         if hasattr(response, 'content'):
             return response.content  
         
@@ -35,6 +40,33 @@ def run_chat(prompt):
         except Exception:
             return f"Error: {e.response.status_code} - {e.response.text}"
 
+    except Exception as e:
+        return f"Error: {str(e)}"
     
+    
+def run_chat_others(prompt):
+    try:
+        response = llm_others.invoke(prompt)
+        if hasattr(response, 'content'):
+            return response.content  
+        
+        return f"Error: Unexpected response format. Response did not contain 'content'."
+        
+    except httpx.HTTPStatusError as e:
+        try:
+            error_json = e.response.json()  
+            error_message = error_json.get("message", "Unknown error")
+
+            if e.response.status_code == 429:
+                return "Error: Requests rate limit exceeded. Please try again later."
+
+            if "too large for model" in error_message:
+                return "Error: Requests rate limit exceeded. Please try again later, break down the input."
+
+            return f"Error: {e.response.status_code} - {error_message}"
+
+        except Exception:
+            return f"Error: {e.response.status_code} - {e.response.text}"
+
     except Exception as e:
         return f"Error: {str(e)}"
